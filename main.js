c2830eb98205b27105dd47b8b60b0a79aa05c780
Dustin Hays <hays.dustin.u@gmail.com>
2023-02-13 03:49:46 -0800

    categories

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 initCategoryRow()//builds categories for Trebekardy boards 
 initBoard() //builds game board
 
+document.querySelector('button').addEventListener('click', buildCategories)
+
+// CREATE CATEGORY ROW
 function initCategoryRow() {
     let catRow = document.getElementById('category-row')
 
@@ -13,6 +16,9 @@ function initCategoryRow() {
     }
 
 }
+
+
+//CREATE CLUE BOARD
 
 function initBoard(){
     let board = document.getElementById('clue-board')
@@ -35,7 +41,72 @@ function initBoard(){
         board.appendChild(row)
     }
 
-    function getClue () {
-        console.log('Have a nice day!')
+    
+}
+
+function randInt () {
+    return Math.floor(Math.random() * (18418) + 1)
+
+}
+
+let catArray = []
+
+// get fetch categories
+
+function buildCategories () {
+
+    if(!(document.getElementById('category-row').firstChild.innerText == '')) {
+        resetBoard()
     }
+
+    const fetchReq1 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq2 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq3 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq4 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq5 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq6 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const allData = Promise.all([fetchReq1,fetchReq2,fetchReq3,fetchReq4,fetchReq5,fetchReq6])
+
+    allData.then((res) => {
+        console.log(res)
+        catArray = res
+        setCategories(catArray)
+    })
+
+}
+
+// LOAD CATEGORIES TO THE BOARD
+
+function setCategories (catArray) {
+    let element = document.getElementById('category-row')
+    let children = element.children;//creates array of all children
+    for(let i = 0; i<children.length; i++){
+        children[i].innerHTML = catArray[i].title
+
+    }
+
+
+}
+
+
+function getClue () {
+    console.log('Have a nice day!')
 }
